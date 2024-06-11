ee7c5b971419fdb227957245fd27ddbda1abb4a6
Jithin Johnson <92211733+centrric-cntc0020@users.noreply.github.com>
2024-06-11 09:58:00 +0530

    app router setup

--- lib/02_view/01_home_page/home_page.dart
+++ lib/02_view/01_home_page/home_page.dart
@@ -1,11 +1,21 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:edu_assign/07_widgets/ww_text.dart';
 import 'package:flutter/material.dart';
 
+@RoutePage()
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: Row(
+      children: [
+        Column(
+          children: [WWText('Hello,'), WWText('Good Morning')],
+        ),
+        const Icon(Icons.menu)
+      ],
+    ));
   }
 }
