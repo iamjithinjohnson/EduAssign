5f2ba9fe5faf115d12c83f508b8ead1e1e3c40c0
Jithin Johnson <92211733+centrric-cntc0020@users.noreply.github.com>
2024-06-11 11:06:24 +0530

    progress on hompage and adding homepage assets

--- lib/02_view/01_home_page/home_page.dart
+++ lib/02_view/01_home_page/home_page.dart
@@ -1,6 +1,9 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:edu_assign/06_utils/constant.dart';
 import 'package:edu_assign/07_widgets/ww_text.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
 
 @RoutePage()
 class HomePage extends StatelessWidget {
@@ -9,13 +12,71 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Row(
-      children: [
-        Column(
-          children: [WWText('Hello,'), WWText('Good Morning')],
+        body: SafeArea(
+      child: Padding(
+        padding: EdgeInsets.all(15.w),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    WWText('Hello,', textSize: TextSize.fw700px28),
+                    WWText('Good Morning', textSize: TextSize.fw400px22)
+                  ],
+                ),
+                const Icon(Icons.menu),
+              ],
+            ),
+            sized0hx50,
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                          height: 216.sp,
+                          child: ColoredBox(
+                              color: const Color(0xffAAC9BF),
+                              child: Column(
+                                children: [
+                                  SvgPicture.asset('assets/svg/book.svg'),
+                                  WWText('Students',
+                                      textSize: TextSize.fw600px17),
+                                ],
+                              ))),
+                    ),
+                    sized0wx10,
+                    Expanded(
+                      child: SizedBox(
+                          height: 216.sp,
+                          child: const ColoredBox(color: Color(0xffD8EBFD))),
+                    )
+                  ],
+                ),
+                sized0hx10,
+                Row(
+                  children: [
+                    Expanded(
+                      child: SizedBox(
+                          height: 216.sp,
+                          child: const ColoredBox(color: Color(0xffFFE0DD))),
+                    ),
+                    sized0wx10,
+                    Expanded(
+                      child: SizedBox(
+                          height: 216.sp,
+                          child: const ColoredBox(color: Color(0xffFFF3D9))),
+                    )
+                  ],
+                )
+              ],
+            )
+          ],
         ),
-        const Icon(Icons.menu)
-      ],
+      ),
     ));
   }
 }
--- lib/07_widgets/ww_text.dart
+++ lib/07_widgets/ww_text.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+enum TextSize {
+  fw700px28,
+  fw700px22,
+  fw600px17,
+  fw400px22,
+  fw400px17,
+  fw400px13,
+}
 
 class WWText extends Text {
   WWText(
@@ -8,14 +18,33 @@ class WWText extends Text {
     double? fontSize,
     FontWeight? fontWeight,
     TextAlign? textAlign,
+    required TextSize textSize,
     TextDecoration? textDecoration,
   }) : super(
-          style: TextStyle(
-            color: color ?? Colors.black,
-            fontSize: fontSize ?? 14.0,
-            fontWeight: fontWeight ?? FontWeight.normal,
-            decoration: textDecoration ?? TextDecoration.none,
-          ),
-          textAlign: textAlign ?? TextAlign.left,
-        );
+            style: customStyle(textSize)
+                .copyWith(decoration: textDecoration ?? TextDecoration.none));
+
+  static TextStyle customStyle(TextSize textSize) {
+    switch (textSize) {
+      case TextSize.fw700px28:
+        return TextStyle(fontSize: 28.sp, fontWeight: FontWeight.w700);
+
+      case TextSize.fw700px22:
+        return TextStyle(fontSize: 22.sp, fontWeight: FontWeight.w700);
+
+      case TextSize.fw600px17:
+        return TextStyle(fontSize: 17.sp, fontWeight: FontWeight.w600);
+
+      case TextSize.fw400px22:
+        return TextStyle(fontSize: 22.sp, fontWeight: FontWeight.w400);
+
+      case TextSize.fw400px17:
+        return TextStyle(fontSize: 17.sp, fontWeight: FontWeight.w400);
+
+      case TextSize.fw400px13:
+        return TextStyle(fontSize: 13.sp, fontWeight: FontWeight.w400);
+      default:
+        return TextStyle(fontSize: 30.sp, fontWeight: FontWeight.w800);
+    }
+  }
 }
